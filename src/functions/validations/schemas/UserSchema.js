419b753840ca6db5208be3f8ff3cecb9163eb6a7
Arthur Bruno <arthur.abbrr@gmail.com>
2021-06-28 20:58:02 -0300

    criação de schema de validação do yup

---
+++ src/functions/validations/schemas/UserSchema.js
@@ -0,0 +1,25 @@
+import * as yup from 'yup';
+
+yup.setLocale({
+  mixed: {
+    required: 'Preencha esse campo para continuar',
+    default: 'Preencha um valor válido',
+  },
+  string: {
+    email: 'Preencha um e-mail válido',
+    min: 'Valor muito curto (mínimo ${min} caracteres)',
+    max: 'Valor muito longo (máximo ${max} caracteres)'
+  },
+  number: {
+    min: 'Valor inválido (deve ser maior ou igual a ${min})',
+    max: 'Valor inválido (deve ser menor ou igual a ${max})'
+  }
+});
+
+export default {
+  name: yup.string().required(),
+  birthDate: yup.string().required(), 
+  email: yup.string().email().required(),
+  address: yup.string().required(),
+  phone: yup.string().min(10).max(11).required(),
+};
